a1d83fb42cce49343b5360be0aa5c9249c1d615a
xalanq <xalanq@gmail.com>
2019-09-05 13:09:31 +0800

    [Update] edit -> user. [Add] getList

--- server.go
+++ server.go
@@ -10,25 +10,11 @@ import (
 	"math/rand"
 	"net/http"
 	"regexp"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 )
-
-var UserCounter int
-var UserArray []User
-var UserTokenMapID = make(map[string]int)
-var UsernameMapID = make(map[string]int)
-var EmailMapID = make(map[string]int)
-
-type User struct {
-	ID       int
-	Username string
-	Password string
-	Email    string
-	Avatar   string
-	Token    string
-}
 
 type ErrorResponse struct {
 	Error   int    `json:"error"`
@@ -44,7 +30,55 @@ const (
 	EmailExist
 	InvalidUsernameOrPassword
 	InvalidAvatar
+	InvalidToken
+	InvalidListType
 )
+
+func webReturnError(w http.ResponseWriter, errorCode int) {
+	var m string
+	switch errorCode {
+	case InvalidForm:
+		m = "错误的表单"
+	case InvalidUsername:
+		m = "用户名必须是大小写字母、数字与下划线，长度在3到20之间"
+	case InvalidPassword:
+		m = "密码必须为可见字符，长度在6到30之间"
+	case InvalidEmail:
+		m = "邮箱格式不合法或者长度超过了100"
+	case UsernameExist:
+		m = "此用户名已被注册"
+	case EmailExist:
+		m = "此邮箱已被注册"
+	case InvalidUsernameOrPassword:
+		m = "用户名或密码错误"
+	case InvalidAvatar:
+		m = "头像链接不合法或长度超过了200"
+	case InvalidToken:
+		m = "无效的token"
+	case InvalidListType:
+		m = "类型只能是category、favorite、history之一"
+	default:
+		m = "未知错误"
+	}
+	json.NewEncoder(w).Encode(ErrorResponse{errorCode, m})
+}
+
+var UserArray = make([]User, 0)
+var UserTokenMapID = make(map[string]int)
+var UsernameMapID = make(map[string]int)
+var EmailMapID = make(map[string]int)
+
+type User struct {
+	ID           int
+	Username     string
+	Password     string
+	Email        string
+	Avatar       string
+	Token        string
+	CategoryList []string
+	FavoriteList []string
+	HistoryList  []string
+}
 
 type RegisterResponse struct {
 	ID    int    `json:"id"`
@@ -96,31 +130,6 @@ func getIDFromToken(token string) (int, bool) {
 	return ID, ok
 }
 
-func webReturnError(w http.ResponseWriter, errorCode int) {
-	var m string
-	switch errorCode {
-	case InvalidForm:
-		m = "错误的表单"
-	case InvalidUsername:
-		m = "用户名必须是大小写字母、数字与下划线，长度在3到20之间"
-	case InvalidPassword:
-		m = "密码必须为可见字符，长度在6到30之间"
-	case InvalidEmail:
-		m = "邮箱格式不合法或者长度超过了100"
-	case UsernameExist:
-		m = "此用户名已被注册"
-	case EmailExist:
-		m = "此邮箱已被注册"
-	case InvalidUsernameOrPassword:
-		m = "用户名或密码错误"
-	case InvalidAvatar:
-		m = "头像链接不合法或长度超过了200"
-	default:
-		m = "未知错误"
-	}
-	json.NewEncoder(w).Encode(ErrorResponse{errorCode, m})
-}
-
 func checkForm(w http.ResponseWriter, r *http.Request) bool {
 	if err := r.ParseForm(); err != nil {
 		log.Println(err)
@@ -169,7 +178,7 @@ func webRegister(w http.ResponseWriter, r *http.Request) {
 	password = fmt.Sprintf("%x", sha256.Sum256([]byte("gggg"+password+"mf")))
 	ID := len(UserArray)
 	token := resetAccessToken(ID, "")
-	user := User{ID, username, password, email, token, avatar}
+	user := User{ID, username, password, email, token, avatar, make([]string, 0), make([]string, 0), make([]string, 0)}
 	UserArray = append(UserArray, user)
 	UsernameMapID[username] = ID
 	EmailMapID[email] = ID
@@ -193,7 +202,7 @@ func webLogin(w http.ResponseWriter, r *http.Request) {
 	webReturnError(w, InvalidUsernameOrPassword)
 }
 
-func webEdit(w http.ResponseWriter, r *http.Request) {
+func webUser(w http.ResponseWriter, r *http.Request) {
 	if !checkForm(w, r) {
 		return
 	}
@@ -209,12 +218,57 @@ func webEdit(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "{}")
 		return
 	}
-	webReturnError(w, InvalidUsernameOrPassword)
+	webReturnError(w, InvalidToken)
+}
+
+func webGetList(w http.ResponseWriter, r *http.Request) {
+	if !checkForm(w, r) {
+		return
+	}
+	token := r.Form.Get("token")
+	listType, ok := validate(r.Form.Get("type"), `(category|favorite|history)`)
+	if !ok {
+		webReturnError(w, InvalidListType)
+		return
+	}
+	skip := 0
+	limit := -1
+	if tmp, err := strconv.Atoi(r.Form.Get("skip")); err == nil && tmp >= 0 {
+		skip = tmp
+	}
+	if tmp, err := strconv.Atoi(r.Form.Get("limit")); err == nil && tmp >= -1 {
+		limit = tmp
+	}
+
+	if ID, ok := getIDFromToken(token); ok {
+		user := UserArray[ID]
+		var retList []string
+		switch listType {
+		case "category":
+			retList = user.CategoryList
+		case "favorite":
+			retList = user.FavoriteList
+		case "history":
+			retList = user.HistoryList
+		}
+		end := skip + limit
+		if limit == -1 || end > len(retList) {
+			end = len(retList)
+		}
+		if skip >= end {
+			json.NewEncoder(w).Encode(make([]string, 0))
+			return
+		}
+		json.NewEncoder(w).Encode(retList[skip:end])
+		return
+	}
+	webReturnError(w, InvalidToken)
 }
 
 func main() {
 	http.HandleFunc("/register", webRegister)
 	http.HandleFunc("/login", webLogin)
-	http.HandleFunc("/edit", webEdit)
+	http.HandleFunc("/user", webUser)
+	http.HandleFunc("/getList", webGetList)
 	log.Fatal(http.ListenAndServe(":18888", nil))
 }
